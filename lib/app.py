2cc7fdd4b54bf0af676fc65487c3b0f82129689b
reuben <rickr6129@gmail.com>
2024-05-20 18:32:42 +0300

    Installing pytest wasn't possible

---
+++ lib/app.py
@@ -0,0 +1,6 @@
+print("Hello world! Pass this test, please.")
+# peace cat 
+print("Hello sun!", end="!")
+print("Hello sky!", end="!!\n")
+print("Hello world!", end="")
+print("Hello sun!", end="\n")
