dfaa00fdcfc02d7e1d0aff14797e887f0656b59d
Rick1Ruben <rickr6129@gmail.com>
2025-04-12 16:09:06 +0300

    blah

--- lib/app.py
+++ lib/app.py
@@ -1,6 +1,2 @@
-print("Hello world! Pass this test, please.")
-# peace cat 
-print("Hello sun!", end="!")
-print("Hello sky!", end="!!\n")
-print("Hello world!", end="")
-print("Hello sun!", end="\n")
+
+print("Hello World! Pass this test, please.")
